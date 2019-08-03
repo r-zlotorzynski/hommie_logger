c1f1e4e27dbba306212cecd59cc0594bbf33fdd6
Rafał Złotorzyński <zlotorzynski.rafal@gmail.com>
2019-08-03 17:40:57 +0200

    Add license file

--- examples/basic_usage/basic_usage.ino
+++ examples/basic_usage/basic_usage.ino
@@ -3,13 +3,23 @@
     @license        MIT
     @contributors   Rafał Złotorzyński
     
-    Licencja, pozwala użytkownikom, o ile zachowane są warunki licencyjne oraz informacje
-    o autorze, w nieograniczonym zakresie na używanie, kopiowanie, rozpowszechnianie, modyfikowanie
-    oryginalnego lub zmodyfikowanego programu w postaci binarnej lub źródłowej.
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-    license that allows users, as long as the license conditions and information about the author
-    are preserved, to an unlimited extent to use, copy, distribute, modify
-    the original or modified program in binary or source form.
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 
     @module         hommie_logger
     @file           ./example/basic/basic_usage.cpp
